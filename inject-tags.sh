1cf194407aa633c7c529513c673b0bcacd569dd9
robjordan <github@jordan-maynard.com>
2020-06-21 14:27:33 +0100

    finished another book - updating scripts

--- inject-tags.sh
+++ inject-tags.sh
@@ -1,1 +1,34 @@
-awk 'BEGIN {print "cat input.txt |"} END {print "cat > output.txt"} {print "sed \x27s/" $0 "/<START:done>&<END>/g\x27 \|"}' < markup.txt > transform.sh
+#! /bin/bash
+# set -x 
+
+usage() {                                      # Function: Print a help message.
+  echo "Usage: $0 [ -v ]" 1>&2 
+}
+
+exit_abnormal() {                              # Function: Exit with error.
+  usage
+  exit 1
+}
+
+VERBOSE=0
+while getopts "v" options; do
+    case "${options}" in                         # 
+        v)                                         # If the option is v,
+            VERBOSE=1
+            ;;
+        *)
+            exit_abnormal
+            ;;
+    esac
+done
+
+awk -F'|' -v verbose=$VERBOSE 'BEGIN {print "cat input.txt |"} 
+    END {print "cat > output.txt"} 
+    {
+        if (verbose) {
+            printf "sed \x27s/%s/<START:done %s>&<END>/g\x27 \x7c\n", $2, $3
+        }
+        else {
+            printf "sed \x27s/%s/<START:done>&<END>/g\x27 \x7c\n", $2
+        }
+    }' < concordance.csv > transform.sh
